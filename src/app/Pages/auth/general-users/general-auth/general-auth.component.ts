7d5a2697ee4b5cade41fe8812fc77985bd096d9c
SakshamPokhrel175 <sakshampokhrel175@gmail.com>
2025-03-26 22:23:22 +0545

    seven commit

--- src/app/Pages/auth/general-users/general-auth/general-auth.component.ts
+++ src/app/Pages/auth/general-users/general-auth/general-auth.component.ts
@@ -1,47 +1,37 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-general-auth',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule , FormsModule],
-  templateUrl: './general-auth.component.html',
-  styleUrls: ['./general-auth.component.css']
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
+  templateUrl: './general-auth.component.html', // Ensure this file exists
+  styleUrls: ['./general-auth.component.css'],
 })
 export class GeneralAuthComponent {
-  loginData = {
-    email: '',
-    password: ''
-  };
-
-  signupData = {
-    email: '',
-    password: '',
-    confirmPassword: ''
-  };
-
-  // Default to Login form
+  loginData = { email: '', password: '' };
+  signupData = { email: '', password: '', confirmPassword: '' };
   isLoginSelected = true;
 
-  // Toggle between Login and Signup forms
   toggleForm(isLogin: boolean) {
     this.isLoginSelected = isLogin;
   }
 
-  // Handle login form submission
   onLogin() {
     console.log('Login form submitted', this.loginData);
-    // Add logic to process login
   }
 
-  // Handle signup form submission
   onSignup() {
     if (this.signupData.password === this.signupData.confirmPassword) {
       console.log('Signup form submitted', this.signupData);
-      // Add logic to process signup
     } else {
       alert('Passwords do not match!');
     }
+  }
+
+  onForgotPassword(event: Event) {
+    event.preventDefault();
+    alert('Forgot password functionality to be implemented.');
   }
 }
